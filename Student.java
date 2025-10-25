266af171cf460169c07cb67a6bf4db821d53f004
GEBRIEL <gebrielherano91@gmail.com>
2025-10-25 01:33:17 -0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,34 @@
+
+public class Student{
+	private String name;
+	private int id;
+	private double gpa;
+	
+	public Student(String name,int id, double gpa) {
+		this.name = name;
+		this.id = id;
+		this.gpa = gpa;	
+	}
+	public String getName() {
+		return name;
+	}
+	public int getId() {
+		return id;
+	}
+	public double getGpa() {
+		return gpa;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setId(int id) {
+		this.id = id;}
+	public void setGpa(double gpa) {
+		this.gpa= gpa;}
+	@Override
+	public String toString() {
+		return " id: " + id  + ", name:"  + name +  ",gpa:" + gpa;
+	}
+}
+	
+
